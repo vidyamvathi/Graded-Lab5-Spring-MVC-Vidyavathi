dcb13fe43c1e07b2c9e70ee8941db64d33c749cd
vidyamvathi <116028518+vidyamvathi@users.noreply.github.com>
2023-07-10 22:57:39 +0530

    Add files via upload

---
+++ ems/src/main/java/com/greatlearning/ems/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.greatlearning.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.ems.entity.Employee;
+
+ 
+
+public interface EmployeeRepository 
+extends JpaRepository <Employee, Long> 
+{
+
+}
---
+++ ems/src/main/java/com/greatlearning/ems/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.greatlearning.ems.service;
+
+import java.util.List;
+
+import com.greatlearning.ems.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> listAllEmployees();
+
+	void saveEmployee(Employee employee);
+
+	Employee getEmployeeDetails(Long id);
+
+	void deleteEmployee(Long id);
+	
+}
